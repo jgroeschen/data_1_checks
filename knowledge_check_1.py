10ad27147ea683fd953699104a58d76c647c38f5
jgroeschen <josephgroeschen@gmail.com>
2022-06-02 13:49:04 -0400

    renamed first knowledge check

---
+++ knowledge_check_1.py
@@ -0,0 +1,21 @@
+print("Hello World!")
+
+
+dogs = ["Spot", "Lassie", "Rover", "Fido"]
+
+print(dogs[1])
+
+
+ages = {
+    "Spot" : 5,
+    "Lassie" : 2,
+    "Rover" : 11,
+    "Fido" : 6
+}
+
+print(ages["Lassie"])
+
+
+colors = ("tan", "black", "white", "brown")
+
+print(colors[0])
